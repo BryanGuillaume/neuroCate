c81a725cf3fc1b1bde79ca172142503689c2bd21
Bryan Guillaume <bryan.guillaume@doct.ulg.ac.be>
2017-05-08 17:47:56 +0800

    change the name of the mask volume info variable for clarity and add the input image path as output

--- neuroCate_3Dimages2Matrix.m
+++ neuroCate_3Dimages2Matrix.m
@@ -1,13 +1,13 @@
-function [Y, maskXYZ_vox, maskXYZ_mm] = neuroCate_3Dimages2Matrix()
+function [Y, maskXYZ_vox, maskXYZ_mm, imagesPath] = neuroCate_3Dimages2Matrix()
 
 imagesPath = spm_select([1 Inf], 'image', 'Select 3D images to analyse');
 maskPath = spm_select(1, 'image', 'Select a mask');
 % maskPath = spm_select([0 1], 'image', 'Select a mask if needed (recommended)');
 
 % if ~isempty(maskPath)
-  VI = spm_vol(maskPath);
-  [mask, maskXYZ_mm] = spm_read_vols(VI);
-  maskXYZ_vox = round(VI.mat \ [maskXYZ_mm; ones(1,prod(VI.dim))]);
+  VM = spm_vol(maskPath);
+  [mask, maskXYZ_mm] = spm_read_vols(VM);
+  maskXYZ_vox = round(VM.mat \ [maskXYZ_mm; ones(1,prod(VM.dim))]);
   maskXYZ_vox = maskXYZ_vox(1:3,:);
   maskXYZ_vox = maskXYZ_vox(:,mask(:) > 0);
   Y = spm_get_data(imagesPath, maskXYZ_vox);
